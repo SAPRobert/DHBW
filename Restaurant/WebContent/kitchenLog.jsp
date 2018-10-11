35c340f30b8e01fae0b3129136750db6b7e6ae37
SPeZiFiiXx <m.hommann98@googlemail.com>
2018-10-11 16:54:27 +0200

    Servlet jeht

---
+++ Restaurant/WebContent/kitchenLog.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Bitte Passwort eingeben!</title>
+</head>
+<body>
+
+<form method="post" action="kitchenLogin">
+  Passwort: <input type="text" name="Passwort"><br>
+  <input type="submit" value="Login">
+</form>
+
+</body>
+</html>
--- Restaurant/src/beanServ/kitchenLogin.java
+++ Restaurant/src/beanServ/kitchenLogin.java
@@ -3,6 +3,7 @@ package beanServ;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,14 +13,15 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Servlet01
  */
-@WebServlet("/Servlet01")
-public class Servlet01 extends HttpServlet {
+@WebServlet("/kitchenLogin")
+public class kitchenLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * Default constructor. 
      */
-    public Servlet01() {
+    public kitchenLogin() {
+        super();
         // TODO Auto-generated constructor stub
     }
 
@@ -28,12 +30,7 @@ public class Servlet01 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		PrintWriter out = response.getWriter();
-		String contextParameterName = "anfangswert01";
-		String contextParameterWert = this.getInitParameter(contextParameterName);
-		out.println("Der Context-Parameter " + contextParameterName + " hat den Wert " +
-		contextParameterWert + ".");
+		doPost(request, response);
 	}
 
 	/**
@@ -41,7 +38,20 @@ public class Servlet01 extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+		String passwort = request.getParameter("Passwort");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		RequestDispatcher rd = null;
+		request.setAttribute(passwort, "hirsch");
+		if(passwort.equals("hirsch")) {
+			rd = request.getRequestDispatcher("/index.jsp");
+			rd.forward(request, response);
+		}
+		else {
+			out.println("<p>Falsches Passwort!</p>");
+			rd = request.getRequestDispatcher("/kitchenLog.jsp");
+			rd.include(request, response);
+		}
+		out.close();
+	}	
 }
